f24ceb306d7cdee5c85c0374e58222f3598269bb
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2007-01-04 16:55:17 +0000

    * Added some skeleton functions  * Made the pref pane universal in deployment configuration  * Added keys necessary for the pref pane to be installed by double clicking on it.

--- CPFPerianPrefPaneController.m
+++ CPFPerianPrefPaneController.m
@@ -2,6 +2,24 @@
 
 @implementation CPFPerianPrefPaneController
 
+- (id)initWithBundle:(NSBundle *)bundle
+{
+    if ( ( self = [super initWithBundle:bundle] ) != nil ) {
+    }
+    
+    return self;
+}
+
+- (void)mainViewDidLoad
+{
+	/* Read prefs here and display */
+}
+
+- (void)didUnselect
+{
+	/* Write prefs here */
+}
+
 - (IBAction)installUninstall:(id)sender
 {
 }
