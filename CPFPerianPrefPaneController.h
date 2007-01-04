493577519223789b905a4e5791508873066ae388
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2007-01-04 18:38:19 +0000

    Added several UI elements to the PrefPane.

--- CPFPerianPrefPaneController.h
+++ CPFPerianPrefPaneController.h
@@ -5,6 +5,39 @@
 
 @interface CPFPerianPrefPaneController : NSPreferencePane
 {
+	//General Pane
+	IBOutlet NSButton					*button_install;
+	IBOutlet NSTextField				*textField_installStatus;
+	IBOutlet NSProgressIndicator		*progress_install;
+	
+	IBOutlet NSTextField				*textField_currentVersion;
+	IBOutlet NSButton					*button_updateCheck;
+	IBOutlet NSProgressIndicator		*progress_updateCheck;
+	IBOutlet NSButton					*button_autoUpdateCheck;
+	
+	//AC3
+	IBOutlet NSButton					*button_ac3DynamicRange;
+	IBOutlet NSButton					*button_ac3StereoOverDolby;
+	
+	//About
+	IBOutlet NSTextView					*textView_about;
+	IBOutlet NSButton					*button_website;
+	IBOutlet NSButton					*button_donate;
+	IBOutlet NSButton					*button_forum;
+	
 }
+//General Pane
 - (IBAction)installUninstall:(id)sender;
+- (IBAction)updateCheck:(id)sender;
+- (IBAction)setAutoUpdateCheck:(id)sender;
+
+//AC3
+- (IBAction)setAC3DynamicRange:(id)sender;
+- (IBAction)setAC3StereoOverDolby:(id)sender;
+
+//About
+- (IBAction)launchWebsite:(id)sender;
+- (IBAction)launchDonate:(id)sender;
+- (IBAction)launchForum:(id)sender;
+
 @end
--- CPFPerianPrefPaneController.m
+++ CPFPerianPrefPaneController.m
@@ -20,8 +20,40 @@
 	/* Write prefs here */
 }
 
+//General Pane
 - (IBAction)installUninstall:(id)sender
 {
 }
+- (IBAction)updateCheck:(id)sender
+{
+}
+
+- (IBAction)setAutoUpdateCheck:(id)sender
+{
+}
+
+
+//AC3
+- (IBAction)setAC3DynamicRange:(id)sender
+{
+}
+
+- (IBAction)setAC3StereoOverDolby:(id)sender
+{
+}
+
+//About
+- (IBAction)launchWebsite:(id)sender
+{
+}
+
+- (IBAction)launchDonate:(id)sender
+{
+}
+
+- (IBAction)launchForum:(id)sender
+{
+}
+
 
 @end
