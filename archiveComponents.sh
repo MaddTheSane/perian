0eadea4c719e26dfc95a75366af9be767ac4a74c
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2007-01-05 16:23:23 +0000

    Even more generic.  Now we can archive components based on where they will be finally installed.  Perian sits in Resources/Components, other Quicktime componets sits in Resources/Components/QuickTime, and Core Audio components sits in Resources/Components/CoreAudio.

--- archiveComponents.sh
+++ archiveComponents.sh
@@ -1,14 +1,25 @@
 #!/bin/sh -v
 PATH=$PATH:/usr/local/bin:/usr/bin:/sw/bin:/opt/local/bin
 
-cd "$BUILT_PRODUCTS_DIR"
-mkdir "PerianPane.prefPane/Contents/Resources/Components"
+function compressComponents(){
+mkdir -p "$2"
+cd $1
 for itemName in *.component ; do
     itemLength=${#itemName}
     item=${itemName:0:$itemLength-10}
-    echo $item
+#    echo $item
     if [[ -d $itemName ]] ; then
         zip -9r "${item}.zip" "$itemName"
-        cp "${item}.zip" "PerianPane.prefPane/Contents/Resources/Components"
+        cp "${item}.zip" "$2"
     fi
 done
+}
+
+
+compressComponents "${BUILT_PRODUCTS_DIR}" "${BUILT_PRODUCTS_DIR}/PerianPane.prefPane/Contents/Resources/Components/"
+if [[ -d "${BUILT_PRODUCTS_DIR}/CoreAudio" ]] ; then
+    compressComponents "${BUILT_PRODUCTS_DIR}/CoreAudio" "${BUILT_PRODUCTS_DIR}/PerianPane.prefPane/Contents/Resources/Components/CoreAudio"
+fi
+if [[ -d "${BUILT_PRODUCTS_DIR}/QuickTime" ]] ; then
+    compressComponents "${BUILT_PRODUCTS_DIR}/QuickTime" "${BUILT_PRODUCTS_DIR}/PerianPane.prefPane/Contents/Resources/Components/QuickTime"
+fi
