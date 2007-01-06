544e75b8d33746130237e23d3845cf7c68674b71
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2007-01-06 17:51:18 +0000

    Added support for building the a52codec external target.

--- archiveComponents.sh
+++ archiveComponents.sh
@@ -1,22 +1,26 @@
 #!/bin/sh -v
 PATH=$PATH:/usr/local/bin:/usr/bin:/sw/bin:/opt/local/bin
 
+function compressAndInsertComponent(){
+itemName="$1"
+itemLength=${#itemName}
+item=${itemName:0:$itemLength-10}
+if [[ -d $itemName ]] ; then
+    ditto -c -k --rsrc --keepParent "$itemName" "${item}.zip"
+    cp "${item}.zip" "$2"
+fi
+}
+
 function compressComponents(){
 mkdir -p "$2"
 cd $1
 for itemName in *.component ; do
-    itemLength=${#itemName}
-    item=${itemName:0:$itemLength-10}
-#    echo $item
-    if [[ -d $itemName ]] ; then
-        ditto -c -k --rsrc --keepParent "$itemName" "${item}.zip"
-        cp "${item}.zip" "$2"
-    fi
+    compressAndInsertComponent $itemName $2
 done
 }
 
-
-compressComponents "${BUILT_PRODUCTS_DIR}" "${BUILT_PRODUCTS_DIR}/PerianPane.prefPane/Contents/Resources/Components/"
+cd "${BUILT_PRODUCTS_DIR}"
+compressAndInsertComponent "Perian.component" "${BUILT_PRODUCTS_DIR}/PerianPane.prefPane/Contents/Resources/Components/"
 if [[ -d "${BUILT_PRODUCTS_DIR}/CoreAudio" ]] ; then
     compressComponents "${BUILT_PRODUCTS_DIR}/CoreAudio" "${BUILT_PRODUCTS_DIR}/PerianPane.prefPane/Contents/Resources/Components/CoreAudio"
 fi
