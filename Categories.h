91c8a6d8cea5bb13a6e0b8732dca52bc6f6221a3
astrange <astrange@621663c8-3916-0410-8f58-edc14a8543d5>
2007-01-30 05:13:06 +0000

    Oops. Add Categories.h/m.

---
+++ Categories.h
@@ -0,0 +1,23 @@
+//
+//  Categories.h
+//  SSAView
+//
+//  Created by Alexander Strange on 1/18/07.
+//  Copyright 2007 Perian Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSCharacterSet (STAdditions)
++ (NSCharacterSet *)newlineCharacterSet;
+@end
+
+@interface NSScanner (STAdditions)
+- (int)scanInt;
+@end
+
+@interface NSString (STAdditions)
+- (NSString *)stringByStandardizingNewlines;
+- (NSArray *)pairSeparatedByString:(NSString *)str;
+- (NSArray *)componentsSeparatedByString:(NSString *)str count:(int)count;
+@end
---
+++ Categories.m
@@ -0,0 +1,71 @@
+//
+//  Categories.m
+//  SSAView
+//
+//  Created by Alexander Strange on 1/18/07.
+//  Copyright 2007 Perian Project. All rights reserved.
+//
+
+#import "Categories.h"
+
+@implementation NSCharacterSet(STUtilities)
++ (NSCharacterSet *)newlineCharacterSet
+{
+	const unichar chars[] = {'\r','\n',0x0085,0x2028,0x2029};
+	return [NSCharacterSet characterSetWithCharactersInString:[NSString stringWithCharacters:chars length:5]];
+}
+@end
+
+@implementation NSScanner (STAdditions)
+- (int)scanInt
+{
+	int r;
+	[self scanInt:&r];
+	return r;
+}
+@end
+
+@implementation NSString (STAdditions)
+- (NSString *)stringByStandardizingNewlines
+{
+	NSMutableString *ms = [NSMutableString stringWithString:self];
+	[ms replaceOccurrencesOfString:@"\r\n" withString:@"\n" options:0 range:NSMakeRange(0,[self length])];
+	[ms replaceOccurrencesOfString:@"\r" withString:@"\n" options:0 range:NSMakeRange(0,[ms length])];
+	return ms;
+}
+
+- (NSArray *)pairSeparatedByString:(NSString *)str
+{
+	NSMutableArray *ar = [NSMutableArray arrayWithCapacity:2];
+	NSRange r = [self rangeOfString:str options:NSLiteralSearch];
+	if (r.length == 0) [ar addObject:self];
+	else {
+		[ar addObject:[self substringToIndex:r.location]];
+		[ar addObject:[self substringFromIndex:r.location + r.length]];
+	}
+	return ar;
+}
+
+- (NSArray *)componentsSeparatedByString:(NSString *)str count:(int)count
+{
+	NSMutableArray *ar = [NSMutableArray arrayWithCapacity:count];
+	NSScanner *sc = [NSScanner scannerWithString:self];
+	NSString *scv;
+	[sc setCharactersToBeSkipped:nil];
+	[sc setCaseSensitive:TRUE];
+	
+	while (count != 1) {
+		count--;
+		[sc scanUpToString:str intoString:&scv];
+		[sc scanString:str intoString:nil];
+		if (scv) [ar addObject:scv]; else [ar addObject:[NSString string]];
+		if ([sc isAtEnd]) break;
+		scv = nil;
+	}
+	
+	[sc scanUpToString:@"" intoString:&scv];
+	if (scv) [ar addObject:scv]; else [ar addObject:[NSString string]];
+
+	return ar;
+}
+@end
