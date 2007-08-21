0615d090d2eefa608d0e54944f06e63976a56269
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2007-08-21 15:08:29 +0000

    * Patch (modified) from Ethan Clark to make it only build the libs for the architectures selected in xcode  * Converted tabs to spaces

--- createStaticLibs.sh
+++ createStaticLibs.sh
@@ -2,18 +2,18 @@
 PATH=$PATH:/usr/local/bin:/usr/bin:/sw/bin:/opt/local/bin
 buildid_ffmpeg="r`svn info ffmpeg | grep -F Revision | awk '{print $2}'`"
 
-generalConfigureOptions="--disable-encoders --disable-muxers --disable-strip --enable-pthreads --disable-ffmpeg --disable-network --disable-ffplay --disable-vhook"
+generalConfigureOptions="--disable-muxers --disable-strip --enable-pthreads --disable-ffmpeg --disable-network --disable-ffplay --disable-vhook"
 
 if [ "$BUILD_STYLE" = "Development" ] ; then
-	generalConfigureOptions="$generalConfigureOptions --disable-opts"
+    generalConfigureOptions="$generalConfigureOptions --disable-opts"
 fi
 
 OUTPUT_FILE="$BUILT_PRODUCTS_DIR/Universal/buildid"
 
 if [[ -e "$OUTPUT_FILE" ]] ; then
-	oldbuildid_ffmpeg=`cat "$OUTPUT_FILE"`
+    oldbuildid_ffmpeg=`cat "$OUTPUT_FILE"`
 else
-	oldbuildid_ffmpeg="buildme"
+    oldbuildid_ffmpeg="buildme"
 fi
 
 QUICKBUILD="$BUILT_PRODUCTS_DIR/Universal/quickbuild"
@@ -23,81 +23,126 @@ if [[ -e "$QUICKBUILD" ]] ; then
 fi
 
 if [[ $buildid == "r" ]] ; then
-	echo "error: you're using svk. Please ask someone to add svk support to the build system. There's a script in Adium svn that can do this."
-	exit 1;
+    echo "error: you're using svk. Please ask someone to add svk support to the build system. There's a script in Adium svn that can do this."
+    exit 1;
+fi
+
+if [ `echo $ARCHS | grep -c i386` -gt 0 ] ; then
+   buildi386=1
+   if [ `echo $ARCHS | grep -c ppc` -gt 0 ] ; then
+       buildppc=1
+   else
+       buildppc=0
+   fi
+elif [ `echo $ARCHS | grep -c ppc` -gt 0 ] ; then
+   buildi386=0
+   buildppc=1
+else
+    echo "No architectures"
+    exit 0
 fi
 
 if [ "$buildid_ffmpeg" = "$oldbuildid_ffmpeg" ] ; then
-	echo "Static ffmpeg libs are up-to-date ; not rebuilding"
+    echo "Static ffmpeg libs are up-to-date ; not rebuilding"
 else
-	echo "Static ffmpeg libs are out-of-date ; rebuilding"
-	
-	mkdir "$BUILT_PRODUCTS_DIR"
-	#######################
-	# Intel shlibs
-	#######################
-	BUILDDIR="$BUILT_PRODUCTS_DIR/intel"
-	mkdir "$BUILDDIR"
-	
-	export optCFlags="-mtune=nocona -fstrict-aliasing -frerun-cse-after-loop -fweb -falign-loops=16 -gstabs+ -mdynamic-no-pic -Dattribute_deprecated=" 
+    echo "Static ffmpeg libs are out-of-date ; rebuilding"
+    
+    echo -n "Building "
+    if [ $buildi386 -eq $buildppc ] ; then
+        echo "Universal"
+    elif [ $buildi386 ] ; then
+        echo "Intel-only"
+    else
+        echo "PPC-only"
+    fi
+    
+    mkdir "$BUILT_PRODUCTS_DIR"
+    #######################
+    # Intel shlibs
+    #######################
+    if [ $buildi386 -gt 0 ] ; then
+        BUILDDIR="$BUILT_PRODUCTS_DIR/intel"
+        mkdir "$BUILDDIR"
 
-	cd "$BUILDDIR"
-	if [ "$oldbuildid_ffmpeg" != "quick" ] ; then
-        if [ `arch` != i386 ] ; then
-            "$SRCROOT/ffmpeg/configure" --cross-compile --arch=i386 --extra-ldflags='-arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk' --extra-cflags='-arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk $optCFlags' $extraConfigureOptions $generalConfigureOptions --cpu=pentium-m 
-        else
-            "$SRCROOT/ffmpeg/configure" $extraConfigureOptions $generalConfigureOptions --cpu=pentium-m --extra-cflags='$optCFlags'
-        fi
+        export optCFlags="-mtune=nocona -fstrict-aliasing -frerun-cse-after-loop -fweb -falign-loops=16 -gstabs+ -mdynamic-no-pic -Dattribute_deprecated=" 
+
+        cd "$BUILDDIR"
+        if [ "$oldbuildid_ffmpeg" != "quick" ] ; then
+            if [ `arch` = ppc ] ; then
+                "$SRCROOT/ffmpeg/configure" --cross-compile --arch=i386 --extra-ldflags='-arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk' --extra-cflags='-arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk $optCFlags' $extraConfigureOptions $generalConfigureOptions --cpu=pentium-m 
+            else
+                "$SRCROOT/ffmpeg/configure" $extraConfigureOptions $generalConfigureOptions --cpu=pentium-m --extra-cflags='$optCFlags'
+            fi
         
-        make depend > /dev/null 2>&1 || true
-    fi
-    if [ "$BUILD_STYLE" = "Development" ] ; then
-        cd libavcodec
-        export CFLAGS="-O1 -fomit-frame-pointer -funit-at-a-time"; make h264.o cabac.o i386/dsputil_mmx.o h264_parser.o
-        unset CFLAGS;
-        cd ..
-    fi
-    make -j3            lib
-	
-	
-	#######################
-	# PPC shlibs
-	#######################
-	BUILDDIR="$BUILT_PRODUCTS_DIR/ppc"
-	mkdir "$BUILDDIR"
-	
-	export optCFlags="-mcpu=G3 -mtune=G5 -fstrict-aliasing -funroll-loops -falign-loops=16 -mmultiple -gstabs+ -mdynamic-no-pic -Dattribute_deprecated="
-	
-	cd "$BUILDDIR"
-	if [ "$oldbuildid_ffmpeg" != "quick" ] ; then
-        if [ `arch` = ppc ] ; then
-            "$SRCROOT/ffmpeg/configure" $extraConfigureOptions $generalConfigureOptions --extra-cflags='$optCFlags'
-        else
-            "$SRCROOT/ffmpeg/configure" --cross-compile --arch=ppc  --extra-ldflags='-arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk' --extra-cflags='-arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk $optCFlags' $extraConfigureOptions $generalConfigureOptions
+            make depend > /dev/null 2>&1 || true
         fi
-        
-        make depend > /dev/null 2>&1 || true
+        if [ "$BUILD_STYLE" = "Development" ] ; then
+            cd libavcodec
+            export CFLAGS="-O1 -fomit-frame-pointer -funit-at-a-time"; make h264.o cabac.o i386/dsputil_mmx.o h264_parser.o
+            unset CFLAGS;
+            cd ..
+        fi
+        make -j3            lib
     fi
-    make -j3            lib
-	
-	#######################
-	# lipo shlibs
-	#######################
-	BUILDDIR="$BUILT_PRODUCTS_DIR/Universal"
-	INTEL="$BUILT_PRODUCTS_DIR/intel"
-	PPC="$BUILT_PRODUCTS_DIR/ppc"
-	rm -rf "$BUILDDIR"
-	mkdir "$BUILDDIR"
-	for aa in "$INTEL"/*/*.a ; do
-		echo lipo -create $aa `echo -n $aa | sed 's/intel/ppc/'` -output `echo -n $aa | sed 's/intel\/.*\//Universal\//'`
-		lipo -create $aa `echo -n $aa | sed 's/intel/ppc/'` -output `echo -n $aa | sed 's/intel\/.*\//Universal\//'`
-	done
-	echo -n "$buildid_ffmpeg" > $OUTPUT_FILE
+    
+    #######################
+    # PPC shlibs
+    #######################
+    if [ $buildppc -gt 0 ] ; then
+        BUILDDIR="$BUILT_PRODUCTS_DIR/ppc"
+        mkdir "$BUILDDIR"
+
+        export optCFlags="-mcpu=G3 -mtune=G5 -fstrict-aliasing -funroll-loops -falign-loops=16 -mmultiple -gstabs+ -mdynamic-no-pic -Dattribute_deprecated="
+    
+        cd "$BUILDDIR"
+        if [ "$oldbuildid_ffmpeg" != "quick" ] ; then
+            if [ `arch` = ppc ] ; then
+                "$SRCROOT/ffmpeg/configure" $extraConfigureOptions $generalConfigureOptions --extra-cflags='$optCFlags'
+            else
+                "$SRCROOT/ffmpeg/configure" --cross-compile --arch=ppc  --extra-ldflags='-arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk' --extra-cflags='-arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk $optCFlags' $extraConfigureOptions $generalConfigureOptions
+            fi
+        
+            make depend > /dev/null 2>&1 || true
+        fi
+        make -j3            lib
+    fi
 fi
+
+#######################
+# lipo/copy shlibs
+#######################
+BUILDDIR="$BUILT_PRODUCTS_DIR/Universal"
+INTEL="$BUILT_PRODUCTS_DIR/intel"
+PPC="$BUILT_PRODUCTS_DIR/ppc"
+
+rm -rf "$BUILDDIR"
+mkdir "$BUILDDIR"
+echo $buildi386 $buildppc
+if [ $buildi386 -eq $buildppc ] ; then
+    # lipo them
+    for aa in "$INTEL"/*/*.a ; do
+        echo lipo -create $aa `echo -n $aa | sed 's/intel/ppc/'` -output `echo -n $aa | sed 's/intel\/.*\//Universal\//'`
+        lipo -create $aa `echo -n $aa | sed 's/intel/ppc/'` -output `echo -n $aa | sed 's/intel\/.*\//Universal\//'`
+    done
+else
+    if [ $buildppc -gt 0 ] ; then
+        archDir="ppc"
+        BUILDARCHDIR=$PPC
+    else
+        archDir="intel"
+        BUILDARCHDIR=$INTEL
+    fi
+    # just copy them
+    for aa in "$BUILDARCHDIR"/*/*.a ; do
+        echo cp "$aa" `echo -n $aa | sed 's/'$archDir'\/.*\//Universal\//'`
+        cp "$aa" `echo -n $aa | sed 's/'$archDir'\/.*\//Universal\//'`
+    done
+fi
+echo -n "$buildid_ffmpeg" > $OUTPUT_FILE
 
 mkdir "$SYMROOT/Universal" || true
 cp "$BUILT_PRODUCTS_DIR/Universal"/* "$SYMROOT/Universal"
 if [ "$BUILD_STYLE" = "Deployment" ] ; then
-	strip -S "$SYMROOT/Universal"/*.a
+    strip -S "$SYMROOT/Universal"/*.a
 fi
 ranlib "$SYMROOT/Universal"/*.a
