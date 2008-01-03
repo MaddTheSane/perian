16c9a5d700c5c4cce40d8b3114dfc2870c5a095a
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2008-01-03 17:30:37 +0000

    Some additional error checking.  Prevents freezing with damaged files, such as the one here: http://appletv.nanopi.net/trac/ticket/75

--- ff_private.c
+++ ff_private.c
@@ -605,7 +605,8 @@ int determine_header_offset(ff_global_ptr storage) {
 		result = av_seek_frame(formatContext, -1, 0, 0);
 		if(result < 0) goto bail;
 		
-		av_read_frame(formatContext, &packet);
+		result = av_read_frame(formatContext, &packet);
+		if(result < 0) goto bail;
 		st = formatContext->streams[packet.stream_index];
 		
 		/* read_packet will give the first decodable packet. However, that isn't necessarily
