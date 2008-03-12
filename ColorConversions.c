bdccf7c4537afe8baaa3ebc60a4c2d95ec9001ad
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2008-03-12 23:31:12 +0000

    A better version of the color conversion asm

--- ColorConversions.c
+++ ColorConversions.c
@@ -209,46 +209,57 @@ static void Y420toY422_sse2(UInt8 *  o, unsigned outRB, unsigned width, unsigned
 		__m128i * ov = (__m128i*)o, * ov2 = (__m128i*)o2, * yv = (__m128i*)yc, * yv2 = (__m128i*)yc2;
 		__m128i * uv = (__m128i*)uc,* vv  = (__m128i*)vc;
 		
+#if 0
+		asm volatile(
+			"\n0:			\n\t"
+			"movdqu		(%4),	%%xmm4	\n\t"
+			"movdqu		(%5),	%%xmm5	\n\t"
+			"addl		$16,	%4		\n\t"
+			"addl		$16,	%5		\n\t"
+			"movdqa		(%2),	%%xmm0	\n\t"
+			"movdqa		16(%2),	%%xmm2	\n\t"
+			"addl		$32,	%2		\n\t"
+			"movdqa		(%3),		%%xmm1	\n\t"
+			"movdqa		16(%3),	%%xmm3	\n\t"
+			"addl		$32,	%3		\n\t"
+			"movdqa		%%xmm4, %%xmm6	\n\t"
+			"punpcklbw	%%xmm5, %%xmm4	\n\t" /*chroma_l*/
+			"punpckhbw	%%xmm5, %%xmm6	\n\t" /*chroma_h*/
+			"movdqa		%%xmm4, %%xmm5	\n\t"
+			"punpcklbw	%%xmm0, %%xmm5	\n\t"
+			"movntdq	%%xmm5, (%0)	\n\t" /*ov[x4]*/
+			"movdqa		%%xmm4, %%xmm5	\n\t"
+			"punpckhbw	%%xmm0, %%xmm5	\n\t"
+			"movntdq	%%xmm5, 16(%0)	\n\t" /*ov[x4+1]*/
+			"movdqa		%%xmm6, %%xmm5	\n\t"
+			"punpcklbw	%%xmm2, %%xmm5	\n\t"
+			"movntdq	%%xmm5, 32(%0)	\n\t" /*ov[x4+2]*/
+			"movdqa		%%xmm6, %%xmm5	\n\t"
+			"punpckhbw	%%xmm2, %%xmm5	\n\t"
+			"movntdq	%%xmm5, 48(%0)	\n\t" /*ov[x4+3]*/
+			"addl		$64,	%0		\n\t"
+			"movdqa		%%xmm4, %%xmm5	\n\t"
+			"punpcklbw	%%xmm1, %%xmm5	\n\t"
+			"movntdq	%%xmm5, (%1)	\n\t" /*ov2[x4]*/
+			"punpckhbw	%%xmm1, %%xmm4	\n\t"
+			"movntdq	%%xmm4, 16(%1)	\n\t" /*ov2[x4+1]*/
+			"movdqa		%%xmm6, %%xmm5	\n\t"
+			"punpcklbw	%%xmm3, %%xmm5	\n\t"
+			"movntdq	%%xmm5, 32(%1)	\n\t" /*ov2[x4+2]*/
+			"punpckhbw	%%xmm3, %%xmm6	\n\t"
+			"movntdq	%%xmm6, 48(%1)	\n\t" /*ov2[x4+3]*/
+			"addl		$64,	%1		\n\t"
+			"dec		%6				\n\t"
+			"jnz		0b				\n\t"
+			:
+			: "r" (ov), "r" (ov2),
+			"r" (yv), "r" (yv2), "r" (uv), "r" (vv),
+			"c" (vWidth)
+			);
+#else
 		for (x = 0; x < vWidth; x++) {
 			unsigned x2 = x*2, x4 = x*4;
 
-#if 1
-			asm volatile(
-				"movdqu		%4,		%%xmm4	\n\t"
-				"movdqu		%5,		%%xmm5	\n\t"
-				"movdqa		%2,		%%xmm0	\n\t"
-				"movdqa	16+1*%2,	%%xmm2	\n\t"
-				"movdqa		%3,		%%xmm1	\n\t"
-				"movdqa	16+1*%3,	%%xmm3	\n\t"
-				"movdqa		%%xmm4, %%xmm6	\n\t"
-				"punpcklbw	%%xmm5, %%xmm4	\n\t" /*chroma_l*/
-				"punpckhbw	%%xmm5, %%xmm6	\n\t" /*chroma_h*/
-				"movdqa		%%xmm4, %%xmm5	\n\t"
-				"punpcklbw	%%xmm0, %%xmm5	\n\t"
-				"movntdq	%%xmm5, %0		\n\t" /*ov[x4]*/
-				"movdqa		%%xmm4, %%xmm5	\n\t"
-				"punpckhbw	%%xmm0, %%xmm5	\n\t"
-				"movntdq	%%xmm5, 16+1*%0	\n\t" /*ov[x4+1]*/
-				"movdqa		%%xmm6, %%xmm5	\n\t"
-				"punpcklbw	%%xmm2, %%xmm5	\n\t"
-				"movntdq	%%xmm5, 32+1*%0	\n\t" /*ov[x4+2]*/
-				"movdqa		%%xmm6, %%xmm5	\n\t"
-				"punpckhbw	%%xmm2, %%xmm5	\n\t"
-				"movntdq	%%xmm5, 48+1*%0	\n\t" /*ov[x4+3]*/
-				"movdqa		%%xmm4, %%xmm5	\n\t"
-				"punpcklbw	%%xmm1, %%xmm5	\n\t"
-				"movntdq	%%xmm5, %1		\n\t" /*ov2[x4]*/
-				"punpckhbw	%%xmm1, %%xmm4	\n\t"
-				"movntdq	%%xmm4, 16+1*%1	\n\t" /*ov2[x4+1]*/
-				"movdqa		%%xmm6, %%xmm5	\n\t"
-				"punpcklbw	%%xmm3, %%xmm5	\n\t"
-				"movntdq	%%xmm5, 32+1*%1	\n\t" /*ov2[x4+2]*/
-				"punpckhbw	%%xmm3, %%xmm6	\n\t"
-				"movntdq	%%xmm6, 48+1*%1	\n\t" /*ov2[x4+3]*/
-				: "=m" (ov[x4]), "=m" (ov2[x4])
-				: "m" (yv[x2]), "m" (yv2[x2]), "m" (uv[x]), "m" (vv[x])
-				);
-#else
 			__m128i	tmp_y = yv[x2], tmp_y3 = yv[x2+1],
 					tmp_y2 = yv2[x2], tmp_y4 = yv2[x2+1],
 					tmp_u = _mm_loadu_si128(&uv[x]), tmp_v = _mm_loadu_si128(&vv[x]),
@@ -264,10 +275,10 @@ static void Y420toY422_sse2(UInt8 *  o, unsigned outRB, unsigned width, unsigned
 			_mm_stream_si128(&ov2[x4+1],_mm_unpackhi_epi8(chroma_l, tmp_y2));
 			_mm_stream_si128(&ov2[x4+2],_mm_unpacklo_epi8(chroma_h, tmp_y4));
 			_mm_stream_si128(&ov2[x4+3],_mm_unpackhi_epi8(chroma_h, tmp_y4));
-#endif
 		}
+#endif
 		
-		for (x = x * 16; x < halfwidth; x++) {
+		for (x = vWidth * 16; x < halfwidth; x++) {
 			unsigned x4 = x*4, x2 = x*2;
 			o2[x4] = o[x4] = uc[x];
 			o[x4 + 1] = yc[x2];
