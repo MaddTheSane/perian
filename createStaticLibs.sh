57470ce7b4a0fd7d9a12032ae43e06e51514b813
astrange <astrange@621663c8-3916-0410-8f58-edc14a8543d5>
2009-05-20 05:27:54 +0000

    - Bump version to 1.1.4b1 - Local patch: don't complain about every h264 file with av_log on

--- createStaticLibs.sh
+++ createStaticLibs.sh
@@ -89,6 +89,7 @@ else
 	patch -p0 < Patches/ffmpeg-no-interlaced.diff
 	patch -p0 < Patches/ffmpeg-faltivec.diff
 	patch -p0 < Patches/ffmpeg-h264-nounrollcabac.diff
+	patch -p0 < Patches/ffmpeg-no-h264-warning.diff
 	touch ffmpeg/patched
 
     echo -n "Building "
