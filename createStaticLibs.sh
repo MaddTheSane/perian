585270d4eb63ba8fee9979719e5e14cf43ea118c
gbooker <gbooker@621663c8-3916-0410-8f58-edc14a8543d5>
2009-10-24 00:36:00 +0000

    Added -D__DARWIN_UNIX03=0 to build of ffmpeg so we use the proper posix definitions to run on 10.4

--- createStaticLibs.sh
+++ createStaticLibs.sh
@@ -7,7 +7,7 @@ if [ "$MACOSX_DEPLOYMENT_TARGET" = "" ]; then
 fi
 
 generalConfigureOptions="--disable-muxers --disable-encoders --disable-stripping --disable-amd3dnow --enable-runtime-cpudetect --enable-pthreads --disable-ffmpeg --disable-network --disable-ffplay --disable-ffserver"
-cflags="-isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -Dattribute_deprecated= -w"
+cflags="-isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -Dattribute_deprecated= -w -D__DARWIN_UNIX03=0"
 
 if [ "$BUILD_STYLE" = "Development" ] ; then
     generalConfigureOptions="$generalConfigureOptions --disable-optimizations --disable-mmx"
