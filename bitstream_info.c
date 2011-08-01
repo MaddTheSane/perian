ae92b9eb5c0cf6dc64970b5ec3ddc8bd0d114215
astrange <astrange@621663c8-3916-0410-8f58-edc14a8543d5>
2011-08-01 04:05:15 +0000

    Correct wrong mFramesPerPacket value for AC3
    Fixes #619

--- bitstream_info.c
+++ bitstream_info.c
@@ -158,7 +158,7 @@ int parse_ac3_bitstream(AudioStreamBasicDescription *asbd, AudioChannelLayout *a
 	asbd->mSampleRate = sample_rate >> shift;
 	asbd->mFormatID = kAudioFormatAC3MS;
 	asbd->mChannelsPerFrame = nfchans_tbl[acmod] + lfe;
-	asbd->mFramesPerPacket = framesize;
+	asbd->mFramesPerPacket = 1536;
 	
 	memset(acl, 0, sizeof(AudioChannelLayout));
 	if(lfe)
