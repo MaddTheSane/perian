46f80f208d81dc2379ba6fb7d9968adc34139c23
C.W. Betts <computers57@hotmail.com>
2013-09-25 02:10:24 -0600

    build the FFMpeg libraries with OpenCL support, although it doesn't look like any of the decoders use it yet. Use loop vectorization in the release build.

--- createStaticLibs.sh
+++ createStaticLibs.sh
@@ -10,7 +10,7 @@ CC=`xcrun -find clang`
 
 configureflags="--cc=$CC --disable-amd3dnow --disable-doc --disable-encoders \
      --disable-muxers --disable-network --disable-programs \
-     --target-os=darwin"
+     --target-os=darwin --enable-opencl"
 
 cflags="-isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -Dattribute_deprecated= -fvisibility=hidden -w"
 
